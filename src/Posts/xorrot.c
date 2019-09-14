8107b7d77c5eb9716fe04c8286a24b785eed13d7
MBR <marc.b.reynolds@gmail.com>
2019-09-14 22:33:49 +0200

    Update xorrot.c

--- src/Posts/xorrot.c
+++ src/Posts/xorrot.c
@@ -7,6 +7,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 // max width to support (2^LOG2_MAX_BITS)
 #define LOG2_MAX_BITS 10
@@ -40,14 +41,6 @@ inline uint64_t rot64(uint64_t x, uint32_t i)
 }
 
 
-#if !defined(_MSC_VER)
-inline __uint128_t rot128(__uint128_t x, uint32_t i)
-{
-  return (x << i) | (x >> ((uint32_t)(-(int32_t)i) & 127));
-}
-#endif
-
-
 // hack
 #if defined(_MSC_VER)
 #include <intrin.h>
@@ -65,15 +58,6 @@ inline uint64_t xor64_rot2(uint64_t x, uint32_t a, uint32_t b)
 {
   return x^rot64(x,a)^rot64(x,b);
 }
-
-#if !defined(_MSC_VER)
-
-inline __uint128_t xor128_rot2(__uint128_t x, uint32_t a, uint32_t b)
-{
-  return x^rot128(x,a)^rot128(x,b);
-}
-#endif
-
 
 // hacky period validatation: just count
 // result is: 32>>ctz(a-b)
@@ -118,7 +102,9 @@ typedef struct {
   uint32_t a,b; 
 } reduce_t;
 
-// for testing
+
+// ***** INVERSE TESTING JUNK
+
 uint32_t inv_32(reduce_t* r, uint32_t x)
 {
   if (r->c != 0) x = rot(x,r->c);
@@ -153,7 +139,18 @@ uint64_t inv_64(reduce_t* r, uint64_t x)
 
 // for testing
 #if !defined(_MSC_VER)
- __uint128_t inv_128(reduce_t* r, __uint128_t x)
+
+inline __uint128_t rot128(__uint128_t x, uint32_t i)
+{
+  return (x << i) | (x >> ((uint32_t)(-(int32_t)i) & 127));
+}
+
+inline __uint128_t xor128_rot2(__uint128_t x, uint32_t a, uint32_t b)
+{
+  return x^rot128(x,a)^rot128(x,b);
+}
+
+__uint128_t inv_128(reduce_t* r, __uint128_t x)
 {
   if (r->c != 0) x = rot128(x,r->c);
 
@@ -169,9 +166,59 @@ uint64_t inv_64(reduce_t* r, uint64_t x)
   
   return x;
 }
+
+#if defined(__clang__)
+
+inline uint8_t rot8(uint64_t x, uint32_t i)
+{
+  return __builtin_rotateleft8(x,i);
+}
+
+inline uint8_t xor8_rot2(uint8_t x, uint32_t a, uint32_t b)
+{
+  return x^rot8(x,a)^rot8(x,b);
+}
+
+uint8_t inv_8(reduce_t* r, uint8_t x)
+{
+  if (r->c != 0) x = rot8(x,r->c);
+
+  switch(r->n) {
+    case 3:  x = xor8_rot2(x,r->term[2].a, r->term[2].b); // not possible (if reduced)
+    case 2:  x = xor8_rot2(x,r->term[1].a, r->term[1].b);
+    default: x = xor8_rot2(x,r->term[0].a, r->term[0].b);
+  }
+  
+  return x;
+}
+
+inline uint16_t rot16(uint64_t x, uint32_t i)
+{
+  return __builtin_rotateleft16(x,i);
+}
+
+inline uint16_t xor16_rot2(uint16_t x, uint32_t a, uint32_t b)
+{
+  return x^rot16(x,a)^rot16(x,b);
+}
+
+uint16_t inv_16(reduce_t* r, uint16_t x)
+{
+  if (r->c != 0) x = rot16(x,r->c);
+
+  switch(r->n) {
+    case 4:  x = xor16_rot2(x,r->term[3].a, r->term[3].b);// not possible (if reduced)
+    case 3:  x = xor16_rot2(x,r->term[2].a, r->term[2].b);
+    case 2:  x = xor16_rot2(x,r->term[1].a, r->term[1].b);
+    default: x = xor16_rot2(x,r->term[0].a, r->term[0].b);
+  }
+  
+  return x;
+}
+#endif
 #endif
 
-
+// ***** END: INVERSE TESTING JUNK
 
 
 // print the expression represented by 'r'
@@ -186,6 +233,7 @@ void reduce_print(reduce_t* r)
   printf("\n");
 }
 
+// in post version for 32-bit
 uint32_t xor_rot2_inv(uint32_t x, uint32_t a, uint32_t b)
 {
   // Perform the five steps (and keep 'a' and 'b' in range given
@@ -207,6 +255,26 @@ uint32_t reduce_validate(uint32_t p, reduce_t* r)
   // inital input 'x' could simply be '1'..the loop
   // is useless (see comment on INVERSE_SAMPLES).
   
+#if defined(__clang__)
+  if (p == 3) {
+    uint8_t x = 0x57;
+    do {
+      uint8_t r0 = xor8_rot2(x, r->a,r->b);
+      uint8_t r1 = inv_8(r,r0);
+      if (r1 != x) return 1;
+      x = r0 * 0x33;
+    } while(--i > 0);
+  } else
+  if (p == 4) {
+    uint16_t x = 0x2457;
+    do {
+      uint16_t r0 = xor16_rot2(x, r->a,r->b);
+      uint16_t r1 = inv_16(r,r0);
+      if (r1 != x) return 1;
+      x = r0 * 0x3457;
+    } while(--i > 0);
+  } else
+#endif  
   if (p == 5) {
     uint32_t x = 0x132457;
     do {
@@ -215,8 +283,8 @@ uint32_t reduce_validate(uint32_t p, reduce_t* r)
       if (r1 != x) return 1;
       x = r0 * 0x123457;
     } while(--i > 0);
-  }
-  else if (p == 6) {
+  } else
+  if (p == 6) {
     uint64_t x = 0x132457;
     do {
       uint64_t r0 = xor64_rot2(x, r->a,r->b);
@@ -228,7 +296,7 @@ uint32_t reduce_validate(uint32_t p, reduce_t* r)
   }
 #if !defined(_MSC_VER)
   else if (p == 7) {
-    uint64_t x = 0x132457;
+    __uint128_t x = 0x132457;
     do {
       __uint128_t r0 = xor128_rot2(x, r->a,r->b);
       __uint128_t r1 = inv_128(r,r0);
@@ -237,12 +305,12 @@ uint32_t reduce_validate(uint32_t p, reduce_t* r)
     } while(--i > 0);
   }
 #endif  
-  else return 1; // not implmented
+  else return 2; // not implmented
   
   return 0;
 }
 
-// yes..this is dumb and doesn't really sort
+// yes..this is dumb
 void bubbleD(pair_t* lo, pair_t* hi)
 {
   uint32_t a,b;
@@ -292,7 +360,7 @@ void reduce(reduce_t* r, uint32_t p, uint32_t a, uint32_t b)
     // if the 'd' goes to zero we're done
     if (b-a == 0) break;
     
-    // if eiter 'a' or 'b' go to zero, then all that's left
+    // if either 'a' or 'b' go to zero, then all that's left
     // is to sum up the solo shift term.
     if (a == 0) { do { c += b; b+=b; b &= mask; } while(b != 0); break; } 
     if (b == 0) { do { c += a; a+=a; a &= mask; } while(a != 0); break; } 
@@ -306,9 +374,10 @@ void reduce(reduce_t* r, uint32_t p, uint32_t a, uint32_t b)
 
   c &= mask;
 
-  // see if we can fold in constant term:  have C^c check if there's (d=bits-c)
-  // (I+C^d+C^e) -> (I + C^c + C^(e+c))
   if (c != 0) {
+    // see if we can fold in constant term:  have C^c check if there's (d=bits-c)
+    // (I+C^d+C^e) -> (I + C^c + C^(e+c))
+    
     uint32_t d = bits-c;
     pair_t*  t = term-1;
 
@@ -319,8 +388,8 @@ void reduce(reduce_t* r, uint32_t p, uint32_t a, uint32_t b)
 	  t->b = c;
 	}
 	else {
-    uint32_t nb = (t->a + c) & mask;
-    t->a = c;
+	  uint32_t nb = (t->a + c) & mask;
+	  t->a = c;
 	  t->b = nb;
 	}
         bubbleD(r->term, t);
@@ -335,10 +404,10 @@ void reduce(reduce_t* r, uint32_t p, uint32_t a, uint32_t b)
   r->c = c;
 }
 
-
+#if defined(BF_FP)  
 // Brute force count the number of fixed point (32-bit). 
 // is equal to: 1<<(1<<ctz(b-a))
-uint32_t bf_count_fixed_point(uint32_t a, uint32_t b)
+uint32_t bf_fp_32(uint32_t a, uint32_t b)
 {
   uint32_t c = 1;   // zero is a fixed point
   uint32_t i = 1;   // start with one
@@ -351,25 +420,64 @@ uint32_t bf_count_fixed_point(uint32_t a, uint32_t b)
   return c;
 }
 
+uint32_t bf_fp_8(uint32_t a, uint32_t b)
+{
+  uint8_t  c = 1;   // zero is a fixed point
+  uint16_t i = 1;   // start with one
+
+  do {
+    uint8_t r = xor8_rot2(i,a,b);
+    if (i == r) c++;
+  } while(++i != 0);
+
+  return c;
+}
+
+uint32_t bf_fp_16(uint32_t a, uint32_t b)
+{
+  uint32_t c = 1;   // zero is a fixed point
+  uint16_t i = 1;   // start with one
+
+  do {
+    uint16_t r = xor16_rot2(i,a,b);
+    if (i == r) c++;
+  } while(++i != 0);
+
+  return c;
+}
+#endif
+
 void xorrot_print_info_header()
 {
-  printf("                       fixed\n");
-  printf("    M        period    points\n");
+  printf("                       fixed    \n");
+  printf("    M        period    points   M^{-1}\n");
   //      (0,000,000):    xxx 
 }
 
+//       0     1   2    3     4
+//  8:  8/2   4/4 2/16
+// 16: 16/2   8/4 4/16 2/256
+// 32: 32/2  16/4 8/16 4/256 2/65536
+
 void xorrot_print_info(uint32_t p, uint32_t a, uint32_t b)
 {
-  uint32_t bits = 1<<p;          // number of bits in integer
+  uint32_t bits = 1<<p;       // number of bits in integer
   uint32_t dz   = ctz(b-a);
-  uint32_t n    = bits>>dz;      // period
-  uint32_t fp   = 1<<(1<<dz);    // number of fixed points
+  uint32_t n    = bits>>dz;   // period
+//uint64_t fp   = UINT64_C(1)<<(UINT64_C(1)<<(dz)); // number of fixed points
+  double   fp   = exp2((float)(1<<dz));  // for longer formats (w/o bothering with the integer)
   reduce_t r;
 
   reduce(&r,p,a,b);
 
+#if defined(BF_FP)  
+  if (p==3) { fp = bf_fp_8 (a,b); } else
+  if (p==4) { fp = bf_fp_16(a,b); } else
+  if (p==5) { fp = bf_fp_32(a,b); }
+#endif  
+
   //      (0,000,000): period f-points
-  printf("(0,%3d,%3d):   %4d %7d ",a,b,n,fp);
+  printf("(0,%3d,%3d):   %4d %7g     ",a,b,n,fp);
   reduce_print(&r);
 }
 
@@ -416,35 +524,51 @@ void test(uint32_t p)
     printf("%d ", h[i]);
   
   if (ifail != 0) {
-    if (ifail != c)
+    if (ifail <= c)
       printf(" ** %d inverse fails**", ifail);
     else
       printf(" ** inverse not tested **");
   }
+
+  
   printf("\n");
 }
-
 
 int main(int argc, char** argv)
 {
   if (argc == 1) {
-    for(uint32_t p=5; p<=LOG2_MAX_BITS; p++)
+    for(uint32_t p=3; p<=LOG2_MAX_BITS; p++)
       test(p);
   }
   else {
     uint32_t bits = atoi(argv[1]);
+    uint32_t mask = bits-1;
     uint32_t p    = ctz(bits);
-    argv += 2;
-    argc -= 2;
 
     if (p <= LOG2_MAX_BITS && (1<<p) == bits) {
       xorrot_print_info_header();
       
-      while(argc >= 2) {
+      if (argc == 2) {
+	// walk though all functions
+	for(uint32_t a=1; a<bits-1; a++) {
+	  for(uint32_t b=a+1; b<=bits-1; b++) {
+	    xorrot_print_info(p,a,b);
+	  }
+	}
+      }
+      else if (argc >= 4) {
+	argv += 2;
 	argc -= 2;
-	uint32_t a = atoi(*argv++);
-	uint32_t b = atoi(*argv++);
-	xorrot_print_info(p,a,b);
+	
+	while(argc >= 2) {
+	  argc -= 2;
+	  uint32_t a = atoi(*argv++) & mask;
+	  uint32_t b = atoi(*argv++) & mask;
+	  if (b < a) { uint32_t t=a; a=b; b=t; }
+	  if (a != b)
+	    xorrot_print_info(p,a,b);
+	  else printf("skipping: (0,%u,%u) = reduces to identity\n",a,b);
+	}
       }
     } else printf("nope..try again\n");  
   }
