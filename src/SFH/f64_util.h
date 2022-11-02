b9f3d1a87fc18f20a1fcf9fbd0634c16729c3fba
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2022-11-03 00:11:52 +0100

    Update f64_util.h

--- src/SFH/f64_util.h
+++ src/SFH/f64_util.h
@@ -20,13 +20,14 @@ extern "C" {
 #if defined(__clang__) && (__clang_major__ >= 13)
 // future me note: GCC 12 is adding: __builtin_assoc_barrier
 
-#define FP_REASSOCIATE_ON  F64_PRAGMA("clang fp reassociate(on)")
-#define FP_REASSOCIATE_OFF F64_PRAGMA("clang fp reassociate(off)")
+#define FP_REASSOCIATE_ON()  F64_PRAGMA("clang fp reassociate(on)")
+#define FP_REASSOCIATE_OFF() F64_PRAGMA("clang fp reassociate(off)")
 #else
-#define FP_REASSOCIATE_ON
-#define FP_REASSOCIATE_OFF
+#define FP_REASSOCIATE_ON()
+#define FP_REASSOCIATE_OFF()
 #endif
 
+// ulp(1.0)
 static const double   f64_ulp1 = 0x1.0p-52;
 
 static const uint64_t f64_sign_bit_k = UINT64_C(1)<<63;
@@ -53,15 +54,26 @@ static inline uint64_t f64_xor(double a, double b)
   return f64_to_bits(a)^f64_to_bits(b);
 }
 
+static inline double f64_mulsign(double v, uint64_t s)
+{
+  return f64_from_bits(f64_to_bits(v)^s);
+}
+
 static inline uint64_t f64_sign_bit(float a)
 {
   return f64_to_bits(a) & f64_sign_bit_k;
 }
 
-// to cut some of the pain stupid math errno not being disabled (groan..please do)
+// to cut some of the pain of math errno not being disabled
+// (-fno-math-errno). But you really should do that. Unless
+// your using visual-c. Then: I feel your pain.
 static inline double f64_sqrt(double a)
 {
-  return __builtin_sqrt(a);
+#if defined(__NO_MATH_ERRNO__) || defined(_M_FP_FAST)
+  return sqrt(a);
+#else  
+  return _mm_cvtsd_f64(_mm_sqrt_sd(_mm_undefined_pd(),_mm_set_sd(a)));
+#endif  
 }
 
 // x >=0 ? 1.0 : -1.f
@@ -76,6 +88,26 @@ static inline double f64_lerp(double t, double a, double b)
 {
   return fma(t,b,-fma(t,a,-a));
 }
+
+static const f64_pair_t f64_up_pi = {.h=0x1.921fb54442d18p1, .l= 0x1.1a62633145c07p-53};
+
+// return RN(x*p) where p is unevaluated pair
+static inline double f64_up_mul(const f64_pair_t* const p, double x) 
+{
+  return fma(x, p->h, x*p->l);
+}
+
+// compute a*b+c
+static inline double f64_up_madd(const f64_pair_t* const a, double b, double c)
+{
+  return fma(a->h, b, fma(a->l, b, c));
+}
+
+static inline double f64_mul_pi(double x)
+{
+  return f64_up_mul(&f64_up_pi,x);
+}
+				
 
 // (a*b) exactly represented by unevaluated pair (h+l)
 // * |l| <= ulp(h)/2
@@ -109,6 +141,13 @@ static inline double f64_pred(double a)
   return f64_from_bits(f64_to_bits(a)-1);
 }
 
+// walks 'd' ULP away from zero
+// * blindly walks past +/-zero to NaN
+static inline double f64_walk(double a, int64_t d)
+{
+  return f64_from_bits(f64_to_bits(a)+(uint64_t)d);
+}
+
 // rounding unit + lowest bit set
 static const double f64_succ_pred_k = 0x1.0000000000001p-53;
 
@@ -133,7 +172,7 @@ static inline double f64_nnz_pred(double a)
 static inline double f64_nnz_next(double a)
 {
   const double s = f64_succ_pred_k; 
-  return fma(fabsf(a),s,a);
+  return fma(fabs(a),s,a);
 }
 
 // next representable FP toward -inf
@@ -141,14 +180,14 @@ static inline double f64_nnz_next(double a)
 static inline double f64_nnz_prev(double a)
 {
   const double s = -f64_succ_pred_k;
-  return fma(fabsf(a),s,a);
+  return fma(fabs(a),s,a);
 }
 
 // ulp(a)
 // * |a| != flt_max
 static inline double f64_ulp(double a)
 {
-  double t = fabsf(a);
+  double t = fabs(a);
   return f64_succ(t)-t;
 }
 
@@ -195,9 +234,9 @@ static inline double f64_mms_e(double a, double b, double c, double d, double* e
 
 // ab+cd: this version returns same results if {a,b} and {c,d} are swapped. It
 // does not improve the error bound.
-FP_REASSOCIATE_OFF
 static inline double f64_mma_c(double a, double b, double c, double d)
 {
+  FP_REASSOCIATE_OFF();
   double p0 = a*b;
   double p1 = c*d;
   double e0 = fma(a,b,-p0);
@@ -232,7 +271,7 @@ static inline uint64_t f64_ulp_dist(double a, double b)
   if ((int64_t)(ub^ua) >= 0)
     return u64_abs(ua-ub);
   
-  return ua+ub+f64_sign_bit;
+  return ua+ub+f64_sign_bit_k;
 }
 
 // a & b are within 'd' ulp of each other
@@ -244,19 +283,19 @@ uint64_t f64_within_ulp(double a, double b, uint64_t d)
   if ((int64_t)(ub^ua) >= 0)
     return ua-ub+d <= d+d;
   
-  return ua+ub+f64_sign_bit+d <= d+d;
+  return ua+ub+f64_sign_bit_k <= d+d;
 }
 
 // filtered approximately equal:
 uint64_t f64_approx_eq(double a, double b, double absD, double relD)
 {
-  double d = fabsf(a-b);
+  double d = fabs(a-b);
   
   if (d <= absD)
     return 1;
   
-  a = fabsf(a);
-  b = fabsf(b);
+  a = fabs(a);
+  b = fabs(b);
   
   double m = (b>a) ? b : a;
   
@@ -268,12 +307,11 @@ uint64_t f64_approx_eq(double a, double b, double absD, double relD)
 
 
 // returns true if a >= b and neither are NaN
-FP_REASSOCIATE_OFF
-static inline bool f64_ordered_ge(double a, double b) { return !(a < b); }
+static inline bool f64_ordered_ge(double a, double b) { FP_REASSOCIATE_OFF(); return !(a < b); }
 
 // returns true if a > b and neither are NaN
-FP_REASSOCIATE_OFF
-static inline bool f64_ordered_gt(double a, double b) { return !(a <= b); }
+
+static inline bool f64_ordered_gt(double a, double b) { FP_REASSOCIATE_OFF(); return !(a <= b); }
 
 // returns true if neither are NaN
 static inline bool f64_are_ordered(double a, double b) { return fpclassify(a+b) != FP_NAN; }
@@ -284,9 +322,9 @@ static inline bool f64_are_ordered(double a, double b) { return fpclassify(a+b) 
 // * |l| <= ulp(h)/2
 // * provided a+b does not overflow
 // * fastmath breaks me
-FP_REASSOCIATE_OFF
 static inline void f64_2sum(f64_pair_t* p, double a, double b)
 {
+  FP_REASSOCIATE_OFF();
   double x  = a+b;
   double bp = x-a;
   double ap = x-bp;
@@ -302,9 +340,9 @@ static inline void f64_2sum(f64_pair_t* p, double a, double b)
 // * |l| <= ulp(h)/2
 // * provided a+b does not overflow
 // * fastmath breaks me
-FP_REASSOCIATE_OFF
 static inline void f64_fast2sum(f64_pair_t* p, double a, double b)
 {
+  FP_REASSOCIATE_OFF();
   double x  = a+b;
   double bp = x-a;
   double y  = b-bp;
