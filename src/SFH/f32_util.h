b3c64216bdddb19b21008a5b563a22f0be984838
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2023-02-06 23:30:39 +0100

    Update f32_util.h

--- src/SFH/f32_util.h
+++ src/SFH/f32_util.h
@@ -1,5 +1,5 @@
 // Public Domain under http://unlicense.org, see link for details.
-// Marc B. Reynolds, 2016-2022
+// Marc B. Reynolds, 2016-2023
 
 #ifndef F32_UTIL_H
 #define F32_UTIL_H
@@ -28,32 +28,54 @@ extern "C" {
 // _M_FP_PRECISE
 // _M_FP_STRICT 
 // GCC macros:
-// __NO_MATH_ERRNO__
-// __RECIPROCAL_MATH__
-// __ASSOCIATIVE_MATH__
+// __FAST_MATH__         // gcc/clang
+// __NO_MATH_ERRNO__     // gcc/clang
+// __RECIPROCAL_MATH__   // gcc (not clang?)
+// __ASSOCIATIVE_MATH__  // gcc (not clang?)
+// __FINITE_MATH_ONLY__  // gcc/clang/intel
+// __SUPPORT_SNAN__      // don't care atm
 
-#if defined(__clang__) && (__clang_major__ >= 13)
-// future me note: GCC 12 is adding: __builtin_assoc_barrier
-
+// kill usage: pita
+#if defined(__clang__) && (__clang_major__ >= 12)
 #define FP32_REASSOCIATE_ON()  F32_PRAGMA("clang fp reassociate(on)")
 #define FP32_REASSOCIATE_OFF() F32_PRAGMA("clang fp reassociate(off)")
-#else
+#define FP32_ASSOC_BARRIER(X)  (X)
+#define FP32_STRICT_FUNC()     
+#elif defined(__GNUC__) &&  (__GNUC__ >= 12)
 #define FP32_REASSOCIATE_ON()
 #define FP32_REASSOCIATE_OFF()
+#define FP32_ASSOC_BARRIER(X)   __builtin_assoc_barrier(X)
+#define FP32_STRICT_FUNC()      // nope...think 
+#else
+"burn baby burn"
+#endif
+
+
+// just say no
+#if (defined(__FAST_MATH__) || defined(__FINITE_MATH_ONLY__) || defined(__ASSOCIATIVE_MATH__) || defined(_M_FP_FAST))
+#define FP32_LOL_NO
 #endif
 
 // wrap an intel intrinsic
 #define F32_SSE_WRAP(F,X) _mm_cvtss_f32(F(_mm_set_ss(X)))
 
+#define F32_SSE_WRAP_B(F,A,B)  _mm_cvtss_f32(F(_mm_set_ss(A),_mm_set_ss(B)))
+#define F32_SSE_WRAP_BI(F,A,B) _mm_cvtsi128_si32(F(_mm_set_ss(A),_mm_set_ss(B)))
+
+
 // ulp(1.f)
 static const float f32_ulp1 = 0x1.0p-23f;
 
-static const uint32_t f32_sign_bit_k = 0x80000000;
+static const uint32_t f32_sign_mask = UINT32_C(0x80000000);
+static const uint32_t f32_mag_mask  = UINT32_C(0x007fffff);
+static const uint32_t f32_exp_mask  = UINT32_C(0x7F800000);
 
 // NOTES:
 // u = round-off unit (2^-24) = ulp(1)/2
 
 typedef struct { float h,l; } f32_pair_t;
+typedef struct { float f; uint32_t u; } f32_u32_tuple_t;
+
 
 static inline uint32_t f32_to_bits(float x)
 {
@@ -79,7 +101,7 @@ static inline float f32_mulsign(float v, uint32_t s)
 
 static inline uint32_t f32_sign_bit(float a)
 {
-  return f32_to_bits(a) & f32_sign_bit_k;
+  return f32_to_bits(a) & f32_sign_mask;
 }
 
 // to cut some of the pain of math errno not being disabled
@@ -94,41 +116,55 @@ static inline float f32_sqrt(float a)
 #elif defined(F32_ARM)
   return __sqrtf(a);
 #else
+  assert(("f32_sqrt: doing nothing", 0));
   // do nothing for warning ATM
 #endif  
 }
 
+// 1/sqrt(a) : lowest error "naive" version
+// SEE: https://gist.github.com/Marc-B-Reynolds/9fb24a7a4ee915e6e973bf9f4d08c404
+// correctly:   86.991060% (14594678)
+// faithfully:  13.008940% (2182538)
+static inline float f32_rsqrt(float a)
+{
+  return f32_sqrt(1.f/a);
+}
 
-// round to nearest (ties to even)
-static inline float f32_round(float x)
+// all seem fine on all modernish targets/compilers
+static inline float f32_ceil(float x)     { return ceilf(x);  }
+static inline float f32_floor(float x)    { return floorf(x); }
+static inline float f32_trunc(float x)    { return truncf(x); }
+static inline float f32_fract(float x)    { return x - f32_trunc(x); }
+
+// intel doesn't have as a op. computed as
+//   truncf(x + copysignf(0x1.fffffep-2,x))  : constant = 0.5-ulp(0.5)
+static inline float f32_round(float x)    { return roundf(x); }
+
+// directed rounding aliases
+// cm=current mode, tz=toward zero, pi=positive inf, ni=negative inf
+static inline float f32_round_cm(float x)  { return nearbyintf(x); }
+static inline float f32_round_tz(float x)  { return f32_trunc(x);  }
+static inline float f32_round_pi(float x)  { return f32_ceil(x);   }
+static inline float f32_round_ni(float x)  { return f32_floor(x);  }
+static inline float f32_round_na(float x)  { return f32_round(x);  }
+
+// round to nearest (ties to even) aka banker's rounding. default mode
+// so 'f32_round_cm' produces same if mode hasn't been changed.
+static inline float f32_round_ne(float x)
 {
 #if defined(F32_INTEL)
   __m128 v = _mm_set_ss(x);
   __m128 r = _mm_round_ss(v,v, _MM_FROUND_TO_NEAREST_INT|_MM_FROUND_NO_EXC);
   
   return _mm_cvtss_f32(r);
-#elif defined(defined(__ARM_FEATURE_DIRECTED_ROUNDING))
+#elif defined(__ARM_FEATURE_DIRECTED_ROUNDING)
   return __rintnf(x);
 #else
+  assert(("f32_round: doing nothing", 0));
   // do nothing for warning ATM
 #endif  
 }
 
-
-#if defined(F32_INTEL)
-
-// round toward zero (doesn't modify sticky flags)
-static inline float f32_round_tz(float x)
-{
-  __m128 v = _mm_set_ss(x);
-  __m128 r = _mm_round_ss(v,v, _MM_FROUND_TO_ZERO|_MM_FROUND_NO_EXC);
-  
-  return _mm_cvtss_f32(r);
-}
-
-#else
-// error ATM
-#endif
 
 // x >=0 ? 1.0 : -1.f
 static inline float f32_sgn(float x) { return copysignf(1.f,x); }
@@ -145,6 +181,16 @@ static inline float f32_lerp(float t, float a, float b)
 
 static const f32_pair_t f32_up_pi = {.h=0x1.921fb6p1, .l=-0x1.777a5cp-24};
 
+// exact product: ab (provided no over/underflow)
+// h = RN(ab), l=RN(ab-RN(ab) -> ab = (h+l)
+static inline void f32_2mul(f32_pair_t* r, float a, float b)
+{
+  float t = a*b;
+  float e = fmaf(a,b,-t);
+  r->h = t;
+  r->l = e;
+}
+
 // return RN(x*p) where p is unevaluated pair
 static inline float f32_up_mul(const f32_pair_t* const p, float x) 
 {
@@ -160,18 +206,6 @@ static inline float f32_up_madd(const f32_pair_t* const a, float b, float c)
 static inline float f32_mul_pi(float x)
 {
   return f32_up_mul(&f32_up_pi,x);
-}
-
-
-// (a*b) exactly represented by unevaluated pair (h+l)
-// * |l| <= ulp(h)/2
-// * provided a+b does not overflow. 
-static inline void f32_2mul(f32_pair_t* p, float a, float b)
-{
-  float x = a*b;
-  float y = fmaf(a,b,-x);
-  p->h = x;
-  p->l = y;
 }
 
 // checks if LSB is clear.
@@ -199,7 +233,7 @@ static inline float f32_pred(float a)
 // * blindly walks past +/-zero to NaN
 static inline float f32_walk(float a, int32_t d)
 {
-  return f32_from_bits(f32_to_bits(a)+d);
+  return f32_from_bits(f32_to_bits(a)+(uint32_t)d);
 }
 
 
@@ -291,7 +325,6 @@ static inline float f32_mms_e(float a, float b, float c, float d, float* e)
 // does not improve the error bound.
 static inline float f32_mma_c(float a, float b, float c, float d)
 {
-  FP32_REASSOCIATE_OFF()
   float p0 = a*b;
   float p1 = c*d;
   float e0 = fmaf(a,b,-p0);
@@ -326,7 +359,7 @@ static inline uint32_t f32_ulp_dist(float a, float b)
   if ((int32_t)(ub^ua) >= 0)
     return u32_abs(ua-ub);
   
-  return ua+ub+f32_sign_bit_k;
+  return ua+ub+f32_sign_mask;
 }
 
 // a & b are within 'd' ulp of each other
@@ -338,7 +371,7 @@ static inline uint32_t f32_within_ulp(float a, float b, uint32_t d)
   if ((int32_t)(ub^ua) >= 0)
     return ua-ub+d <= d+d;
   
-  return ua+ub+f32_sign_bit_k+d <= d+d;
+  return ua+ub+f32_sign_mask+d <= d+d;
 }
 
 // filtered approximately equal:
@@ -360,18 +393,26 @@ static inline uint32_t f32_approx_eq(float a, float b, float absD, float relD)
   return 0;
 }
 
-
-// returns true if a >= b and neither are NaN
-
-static inline bool f32_ordered_ge(float a, float b) { FP32_REASSOCIATE_OFF(); return !(a < b); }
-
-// returns true if a > b and neither are NaN
-static inline bool f32_ordered_gt(float a, float b) { FP32_REASSOCIATE_OFF(); return !(a <= b); }
-
 // returns true if neither are NaN
-static inline bool f32_are_ordered(float a, float b) { return fpclassify(a+b) != FP_NAN; }
+static inline bool f32_are_ordered(float a, float b)
+{ 
+  FP32_REASSOCIATE_OFF();
+  return (a==a) && (b==b);
+}
 
+// returns true if a >= b or neither are NaN
+static inline bool f32_ge_or_unordered(float a, float b)
+{
+  FP32_REASSOCIATE_OFF();
+  return !(a < b);
+}
 
+// returns true if a > b or neither are NaN
+static inline bool f32_gt_or_unordered(float a, float b)
+{
+  FP32_REASSOCIATE_OFF();
+  return !(a <= b);
+}
 
 // (a+b) exactly represented by unevaluated pair (h+l)
 // * |l| <= ulp(h)/2
@@ -380,12 +421,12 @@ static inline bool f32_are_ordered(float a, float b) { return fpclassify(a+b) !=
 static inline void f32_2sum(f32_pair_t* p, float a, float b)
 {
   FP32_REASSOCIATE_OFF();
-  float x  = a+b;
-  float bp = x-a;
-  float ap = x-bp;
-  float eb = b-bp;
-  float ea = a-ap;
-  float y  = ea+eb;
+  float x  = (a+b);
+  float bp = (x-a);
+  float ap = (x-bp);
+  float eb = (b-bp);
+  float ea = (a-ap);
+  float y  = (ea+eb);
   p->h = x;
   p->l = y;
 }
@@ -398,15 +439,16 @@ static inline void f32_2sum(f32_pair_t* p, float a, float b)
 static inline void f32_fast2sum(f32_pair_t* p, float a, float b)
 {
   FP32_REASSOCIATE_OFF();
-  float x  = a+b;
-  float bp = x-a;
-  float y  = b-bp;
+  float x  = (a+b);
+  float bp = (x-a);
+  float y  = (b-bp);
   p->h = x;
   p->l = y;
 }
 
+
 #ifdef __cplusplus
-extern "C" {
+}
 #endif
 #endif
 
