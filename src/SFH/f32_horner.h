687ee3ffa832880f48293dca5b95d153653436bc
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2023-02-18 10:33:25 +0100

    Update f32_horner.h

--- src/SFH/f32_horner.h
+++ src/SFH/f32_horner.h
@@ -3,15 +3,10 @@
 
 // evalulate n^th degree polynomial in Horner's form. Coefficient arrays are from high to low (c_n,... c_0)
 
-#ifndef __F32_HORNER__
-#define __F32_HORNER__
+#ifndef F32_HORNER
+#define F32_HORNER
 
-#ifdef __cplusplus
-extern "C" {
-#ifdef __emacs_hack
-}  
-#endif  
-#endif
+typedef float (f32_horner_t)(float, const float * const);
 
 static inline float f32_horner_1(float x, const float C[static 2])
 {
@@ -32,11 +27,6 @@ static inline float f32_horner_12(float x, const float C[static 13]) { return fm
 static inline float f32_horner_13(float x, const float C[static 14]) { return fmaf(x,f32_horner_12(x,C),C[13]); }
 static inline float f32_horner_14(float x, const float C[static 15]) { return fmaf(x,f32_horner_13(x,C),C[14]); }
 static inline float f32_horner_15(float x, const float C[static 16]) { return fmaf(x,f32_horner_14(x,C),C[15]); }
-
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
 
