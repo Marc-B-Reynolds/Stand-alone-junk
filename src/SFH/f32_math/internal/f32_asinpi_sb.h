cfb335b20300d1d10e78e6c8f2db9c40d74951eb
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2023-02-20 10:26:07 +0100

    Update f32_asinpi_sb.h

--- src/SFH/f32_math/internal/f32_asinpi_sb.h
+++ src/SFH/f32_math/internal/f32_asinpi_sb.h
@@ -2,37 +2,67 @@
 #ifndef __F32_ASINPI_SB_H__
 #define __F32_ASINPI_SB_H__
 
-// less than ideal numerically. simply converts acospi to asinpi by
-// asinpi(x) = 1/2 - acospi(x)
-// but we're spitballing kids!
-static inline float f32_asinpi_sb_p(float (*f)(float), float x)
+// fast branchfree asinpi targeting minimizing abs error. intended for
+// low quality approximations.
+// SEE:
+// * (put post link here)
+// * f32_acospi_sb.h
+//
+// implementation is simply:
+//    asinpi(x) = 1/2 - acospi(x).
+//
+// which is awful numerically. A better solution would be a cheap
+// direct form but a good expression is alluding me. An example is
+// that RN(acospi(x)) = 1/2 on [-0x1.921fb4p-24, 0x1.921fb4p-25]
+// so the asinpi approximation on this range will be zero instead
+// of ~x/pi. But spitballing and abs error (shrug)!
+
+// given f = P(x) expand approximation restricted to positive input.
+// return NaN for negative inputs.
+// just hacked: now test blockhead
+static inline float f32_asinpi_sb_xp(float (*f)(float), float x)
 {
-#if defined(ARG_REDUCE_STDFUNCS)
-  // using standard functions
+  float t = f32_sqrt(1.f-x);
+  float p = f(x);
 
-  float a = fabsf(x);
-  float c = copysignf(0.5f, x);
-  float t = -sqrtf(1.f-a);
-  float p = f(a);
-  
-  return fmaf(t, copysignf(p,x), c);
-  
-#else
-  uint32_t ix = f32_to_bits(x);        // IEEE bit pattern of x
-  uint32_t ax = ix & 0x7fffffff;       // IEEE bit pattern of |x|
-  uint32_t sx = ix ^ ax;               // sign bit isolated
-  float    a  = f32_from_bits(ax);     // fabsf(x)
-  float    t  = -sqrtf(1.f-a);         // the transform
-  float    c  = f32_mulsign(0.5f, sx); // the constant
-  float    p  = f(a);                  // expand the polynomial P(x)
+  return -fmaf(t,p,-0.5f);
+}
 
+// given f = P(x) expand full range approximation:
+//    asinpi(x) = sign(x)(1/2 - sqrt(1-|x|)) P(|x|)
+static inline float f32_asinpi_sb_xf(float (*f)(float), float x)
+{
+  // currently only hybrid reduction: (SEE: f32_odd_reduce)
+  // as it generally works well with current Intel & ARM arches
+
+  float a  = fabsf(x);
+  float sx = f32_xor(x,    a);
+  float c  = f32_xor(0.5f, sx);
+  float t  = -f32_sqrt(1.f-a);
+  float p  = f(a);
+  
   // combine the subresults: here the sign is being applied the polynomial
   // result 'p' but could instead be applied to sqrt result 't'. ideally
   // it would applied to the sub-result that's expected to complete first.
-  // roughly very low degree P apply to 'p', otherwise to 't'
-  
-  return fmaf(t, f32_mulsign(p, sx), c);
-#endif  
+  // roughly very low degree P apply to 'p', otherwise to 't' (as below)
+
+  return fmaf(t, f32_xor(p,sx), c);
 }
+
+
+// longer polynomial version (see note above)
+// just hacked: now test blockhead
+static inline float f32_asinpi_sb_xf_l(float (*f)(float), float x)
+{
+  float a  = fabsf(x);
+  float sx = f32_xor(x,    a);
+  float c  = f32_xor(0.5f, sx);
+  float t  = -f32_sqrt(1.f-a);
+  float p  = f(a);
+
+  return fmaf(f32_xor(t,sx), p, c);
+}
+
+
 
 #endif
