b15c35afbd0fd9cc52d08eb85d54b1e506032eb3
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2023-02-25 18:06:28 +0100

    Create f32_asincos.c

---
+++ src/SFH/f32_math/f32_asincos.c
@@ -0,0 +1,15 @@
+// Public Domain under http://unlicense.org, see link for details.
+// Marc B. Reynolds, 2022-2023
+
+#include "f32_utils.h"
+
+
+// SEE:
+// * 
+
+#include "internal/f32_math_common.h"
+#include "internal/f32_asincos.h"
+
+
+
+
