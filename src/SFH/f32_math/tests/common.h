83821453c35a48741875b892e9fb8684eb0cb771
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2023-03-01 16:56:10 +0100

    Update common.h

--- src/SFH/f32_math/tests/common.h
+++ src/SFH/f32_math/tests/common.h
@@ -98,7 +98,8 @@ void test_force(uint32_t x0, uint32_t x1)
   float f0 = f32_from_bits(x0);
   float f1 = f32_from_bits(x1);
   
-  printf("\nchecking: %s on [%08x,%08x] [%e,%e]\n", func_name, x0,x1,f0,f1);
+  printf("<details markdown=\"1\">\n\n");
+  printf("<summary>checking: %s on [%08x,%08x] [%e,%e]<\\summary>\n\n", func_name, x0,x1,f0,f1);
 
   func_error_t error[LENGTHOF(func_table)] = {{0}};
   
@@ -114,6 +115,7 @@ void test_force(uint32_t x0, uint32_t x1)
   
   error_to_totals(error);
   error_dump_i(error);
+  printf("<\\details>\n");
 }
 
 
@@ -125,7 +127,8 @@ void test_linear_range(uint32_t x0, uint32_t x1, float k)
   float f0 = f32_from_bits(x0);
   float f1 = f32_from_bits(x1);
   
-  printf("\nchecking: %s on [%08x,%08x] [%e,%e] : {linear result range %a*x}\n", func_name, x0,x1,f0,f1,k);
+  printf("<details markdown=\"1\"><summary>");
+  printf("\n<summary>checking: %s on [%08x,%08x] [%e,%e] : {linear result range %a*x}<\\summary>\n", func_name, x0,x1,f0,f1,k);
   
   for(uint32_t fi=0; fi < LENGTHOF(func_table); fi++) {
     for(uint32_t xi=x0; xi<=x1; xi++) {
@@ -137,6 +140,7 @@ void test_linear_range(uint32_t x0, uint32_t x1, float k)
   }
   error_to_totals(error);
   error_dump_i(error);
+  printf("\n<details>\n");
 }
 
 
@@ -147,8 +151,9 @@ void test_const_range(uint32_t x0, uint32_t x1, float k)
   
   float f0 = f32_from_bits(x0);
   float f1 = f32_from_bits(x1);
-  
-  printf("\nchecking: %s on [%08x,%08x] [%e,%e] : {constant result range %a}\n", func_name, x0,x1,f0,f1,k);
+
+  printf("<details markdown=\"1\"><summary>");
+  printf("\nchecking: %s on [%08x,%08x] [%e,%e] : {constant result range %a}<\\summary>\n", func_name, x0,x1,f0,f1,k);
   
   for(uint32_t fi=0; fi < LENGTHOF(func_table); fi++) {
     for(uint32_t xi=x0; xi<=x1; xi++) {
@@ -159,6 +164,7 @@ void test_const_range(uint32_t x0, uint32_t x1, float k)
   }
   error_to_totals(error);
   error_dump_i(error);
+  printf("\n<details>\n");
 }
 
 // correctly rounded f(x) = RN(RN(kx,53),24) on [x0,x1]
@@ -169,7 +175,8 @@ void test_linear_range_dp_up(uint32_t x0, uint32_t x1, const f64_pair_t* k)
   float f0 = f32_from_bits(x0);
   float f1 = f32_from_bits(x1);
   
-  printf("\nchecking: %s on [%08x,%08x] [%e,%e] : {linear result range}\n", func_name, x0,x1,f0,f1);
+  printf("<details markdown=\"1\"><summary>");
+  printf("\nchecking: %s on [%08x,%08x] [%e,%e] : {linear result range}<\\summary>\n", func_name, x0,x1,f0,f1);
   
   for(uint32_t fi=0; fi < LENGTHOF(func_table); fi++) {
     for(uint32_t xi=x0; xi<=x1; xi++) {
@@ -181,6 +188,7 @@ void test_linear_range_dp_up(uint32_t x0, uint32_t x1, const f64_pair_t* k)
   }
   error_to_totals(error);
   error_dump_i(error);
+  printf("\n<details>\n");
 }
 
 // correctly rounded f(x) = x on [x0,x1]
@@ -191,7 +199,8 @@ void test_identity_range(uint32_t x0, uint32_t x1)
   float f0 = f32_from_bits(x0);
   float f1 = f32_from_bits(x1);
   
-  printf("\nchecking: %s on [%08x,%08x] [%e,%e] : {result = input range} \n", func_name, x0,x1,f0,f1);
+  printf("<details markdown=\"1\"><summary>");
+  printf("\nchecking: %s on [%08x,%08x] [%e,%e] : {result = input range}<\\summary>\\n", func_name, x0,x1,f0,f1);
 
   for(uint32_t fi=0; fi < LENGTHOF(func_table); fi++) {
     for(uint32_t xi=x0; xi<=x1; xi++) {
@@ -203,6 +212,7 @@ void test_identity_range(uint32_t x0, uint32_t x1)
 
   error_to_totals(error);
   error_dump_i(error);
+  printf("\n<details>\n");
 }
 
 // test all denormal inputs
