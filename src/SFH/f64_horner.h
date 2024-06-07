98d546f8b84816be57ca179c33b2dcc549340928
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2024-06-07 19:24:25 +0200

    Update f64_horner.h

--- src/SFH/f64_horner.h
+++ src/SFH/f64_horner.h
@@ -1,5 +1,5 @@
 // Public Domain under http://unlicense.org, see link for details.
-// Marc B. Reynolds, 2015-2023
+// Marc B. Reynolds, 2015-2024
 
 // evalulate n^th degree polynomial in Horner's form. Coefficient arrays are from high to low.
 
