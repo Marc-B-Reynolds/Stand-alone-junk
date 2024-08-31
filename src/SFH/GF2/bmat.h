4c800521f17d86a5814e0737810b0c33b746c46f
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2024-08-31 22:42:49 +0200

    Create bmat.h

---
+++ src/SFH/GF2/bmat.h
@@ -0,0 +1,127 @@
+// Marc B. Reynolds, 2022-2024
+// Public Domain under http://unlicense.org, see link for details.
+
+#pragma once
+
+#include <stdint.h>
+#include <string.h>
+
+#include "prng_small.h" // temp hack
+
+// hide your eyes
+
+// number of 64-bit words
+#define BMAT_WSIZE_8   1
+#define BMAT_WSIZE_16  4
+#define BMAT_WSIZE_32 16
+#define BMAT_WSIZE_64 64
+
+#define bmat_def_8(X)  uint64_t X[ 1]
+#define bmat_def_16(X) uint64_t X[ 4]
+#define bmat_def_32(X) uint64_t X[16]
+#define bmat_def_64(X) uint64_t X[64]
+
+#if !defined(_MSC_VER)
+#define bmat_rparam_n(X)  uint64_t* restrict X
+
+#define bmat_rparam_8(X)  uint64_t X[restrict static  1]
+#define bmat_rparam_16(X) uint64_t X[restrict static  4]
+#define bmat_rparam_32(X) uint64_t X[restrict static 16]
+#define bmat_rparam_64(X) uint64_t X[restrict static 64]
+
+#define bmat_param_8(X)  uint64_t X[static  1]
+#define bmat_param_16(X) uint64_t X[static  4]
+#define bmat_param_32(X) uint64_t X[static 16]
+#define bmat_param_64(X) uint64_t X[static 64]
+
+#define bmat_array_8(X)  uint8_t  X[static  8]
+#define bmat_array_16(X) uint16_t X[static 16]
+#define bmat_array_32(X) uint32_t X[static 32]
+#define bmat_array_64(X) uint64_t X[static 64]
+
+#else
+#define bmat_rparam_n(X)  uint64_t* X
+
+#define bmat_rparam_8(X)  uint64_t* X
+#define bmat_rparam_16(X) uint64_t* X
+#define bmat_rparam_32(X) uint64_t* X
+#define bmat_rparam_64(X) uint64_t* X
+
+#define bmat_param_8(X)  uint64_t* X
+#define bmat_param_16(X) uint64_t* X
+#define bmat_param_32(X) uint64_t* X
+#define bmat_param_64(X) uint64_t* X
+
+#define bmat_array_8(X)  uint8_t*  X
+#define bmat_array_16(X) uint16_t* X
+#define bmat_array_32(X) uint32_t* X
+#define bmat_array_64(X) uint64_t* X
+#endif
+
+static inline void bmat_dup_8      (bmat_param_8(a),  bmat_param_8(m))  { memcpy(a, m,   8); }
+static inline void bmat_dup_16     (bmat_param_16(a), bmat_param_16(m)) { memcpy(a, m,  32); }
+static inline void bmat_dup_32     (bmat_param_32(a), bmat_param_32(m)) { memcpy(a, m, 128); }
+static inline void bmat_dup_64     (bmat_param_64(a), bmat_param_64(m)) { memcpy(a, m, 512); }
+
+// bmat_to_array_n/array_to_bmat_n :
+// * for type-punning between array types (for strict aliasing)
+// * helpful for locating where endian issues will occur (see documentation..which I haven't written yet)
+static inline void bmat_to_array_8 (bmat_array_8 (a), bmat_param_8 (m)) { memcpy(a, m,   8); }
+static inline void bmat_to_array_16(bmat_array_16(a), bmat_param_16(m)) { memcpy(a, m,  32); }
+static inline void bmat_to_array_32(bmat_array_32(a), bmat_param_32(m)) { memcpy(a, m, 128); }
+static inline void bmat_to_array_64(bmat_array_64(a), bmat_param_64(m)) { memcpy(a, m, 512); }
+
+static inline void array_to_bmat_8 (bmat_param_8 (m), bmat_array_8 (a)) { memcpy(m, a,   8); }
+static inline void array_to_bmat_16(bmat_param_16(m), bmat_array_16(a)) { memcpy(m, a,  32); }
+static inline void array_to_bmat_32(bmat_param_32(m), bmat_array_32(a)) { memcpy(m, a, 128); }
+static inline void array_to_bmat_64(bmat_param_64(m), bmat_array_64(a)) { memcpy(m, a, 512); }
+
+// direct 8x8 as uint64_t
+static inline uint64_t bmat_add_8_i  (uint64_t a, uint64_t b) { return (a^b);    }
+static inline bool     bmat_equal_8_i(uint64_t a, uint64_t b) { return (a^b)==0; }
+
+// bmat_basics.c
+extern bool bmat_equal_8 (bmat_param_8 (a), bmat_param_8 (b));
+extern bool bmat_equal_16(bmat_param_16(a), bmat_param_16(b));
+extern bool bmat_equal_32(bmat_param_32(a), bmat_param_32(b));
+extern bool bmat_equal_64(bmat_param_64(a), bmat_param_64(b));
+
+extern void bmat_add_8 (bmat_rparam_8 (d), bmat_param_8 (a), bmat_param_8 (b));
+extern void bmat_add_16(bmat_rparam_16(d), bmat_param_16(a), bmat_param_16(b));
+extern void bmat_add_32(bmat_rparam_32(d), bmat_param_32(a), bmat_param_32(b));
+extern void bmat_add_64(bmat_rparam_64(d), bmat_param_64(a), bmat_param_64(b));
+
+extern void bmat_sum_8 (bmat_param_8 (d), bmat_param_8 (a));
+extern void bmat_sum_16(bmat_param_16(d), bmat_param_16(a));
+extern void bmat_sum_32(bmat_param_32(d), bmat_param_32(a));
+extern void bmat_sum_64(bmat_param_64(d), bmat_param_64(a));
+
+extern uint32_t bmat_md_sum_8 (bmat_param_8 (m));
+extern uint32_t bmat_md_sum_16(bmat_param_16(m));
+extern uint32_t bmat_md_sum_32(bmat_param_32(m));
+
+extern uint32_t bmat_trace_8 (bmat_param_8 (m));
+extern uint32_t bmat_trace_16(bmat_param_16(m));
+extern uint32_t bmat_trace_32(bmat_param_32(m));
+
+// bmat_gaussian.c
+extern uint32_t bmat_rank_8 (bmat_param_8 (m));
+extern uint32_t bmat_rank_16(bmat_param_16(m));
+extern uint32_t bmat_rank_32(bmat_param_32(m));
+extern uint32_t bmat_rank_64(bmat_param_64(m));
+
+extern bool bmat_is_full_rank_8 (bmat_param_8 (m));
+extern bool bmat_is_full_rank_16(bmat_param_16(m));
+extern bool bmat_is_full_rank_32(bmat_param_32(m));
+extern bool bmat_is_full_rank_64(bmat_param_64(m));
+
+// F_2 unary & binary function types
+typedef uint32_t (f2_ufunc_32_t)(uint32_t);
+typedef uint64_t (f2_ufunc_64_t)(uint64_t);
+typedef uint32_t (f2_bfunc_32_t)(uint32_t, uint32_t);
+typedef uint64_t (f2_bfunc_64_t)(uint64_t, uint64_t);
+
+extern uint32_t bmat_from_ufunc_32  (bmat_param_32(m), f2_ufunc_32_t* f);
+extern uint64_t bmat_from_ufunc_64  (bmat_param_64(m), f2_ufunc_64_t* f);
+extern uint32_t bmat_from_ufunc_p_32(bmat_param_64(m), f2_bfunc_32_t* f, uint32_t k);
+extern uint64_t bmat_from_ufunc_p_64(bmat_param_64(m), f2_bfunc_64_t* f, uint64_t k);
