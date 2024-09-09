7393ad795770da93e251564246ff97b37188350e
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2024-09-09 11:33:13 +0200

    Create bmat_everything.h

---
+++ src/SFH/GF2/bmat_everything.h
@@ -0,0 +1,19 @@
+// Marc B. Reynolds, 2022-2024
+// Public Domain under http://unlicense.org, see link for details.
+
+// just "include every source file here" + "include this" and VOILA!
+// SRLY: for no-build system (no configuration communication), multi-config + easy LTO.
+// sure it's evil but (shrug) not a library.
+
+#include "bmat_i.h"
+#include "bmat_basics.c"
+#include "bmat_set.c"
+#include "bmat_block.c"
+#include "bmat_gauss.c"
+#include "bmat_mul.c"
+#include "bmat_transpose.c"
+#include "bmat_random.c"
+
+#include "bmat_ref.c"
+#include "bmat_m4ri.c"
+#include "bmat_print.c"
