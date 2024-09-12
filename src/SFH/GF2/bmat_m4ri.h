7c32adb5443babf48d4d549fa3431319b9bf1353
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2024-09-12 14:18:08 +0200

    Update bmat_m4ri.h

--- src/SFH/GF2/bmat_m4ri.h
+++ src/SFH/GF2/bmat_m4ri.h
@@ -27,3 +27,32 @@ extern mzd_t* m4ri_make_8 (bmat_param_8 (s));
 extern mzd_t* m4ri_make_16(bmat_param_16(s));
 extern mzd_t* m4ri_make_32(bmat_param_32(s));
 extern mzd_t* m4ri_make_64(bmat_param_64(s));
+
+extern void m4ri_set_rv_8 (mzd_t* v, uint8_t  x);
+extern void m4ri_set_rv_16(mzd_t* v, uint16_t x);
+extern void m4ri_set_rv_32(mzd_t* v, uint32_t x);
+extern void m4ri_set_rv_64(mzd_t* v, uint64_t x);
+
+extern uint32_t m4ri_get_rv_8 (mzd_t* v);
+extern uint32_t m4ri_get_rv_16(mzd_t* v);
+extern uint32_t m4ri_get_rv_32(mzd_t* v);
+extern uint64_t m4ri_get_rv_64(mzd_t* v);
+
+// heavy weight wrappers
+extern void m4ri_wrap_mm_8 (bmat_param_8(C),  bmat_param_8(A),  bmat_param_8(B));
+extern void m4ri_wrap_mm_16(bmat_param_16(C), bmat_param_16(A), bmat_param_16(B));
+extern void m4ri_wrap_mm_32(bmat_param_32(C), bmat_param_32(A), bmat_param_32(B));
+extern void m4ri_wrap_mm_64(bmat_param_64(C), bmat_param_64(A), bmat_param_64(B));
+extern void m4ri_wrap_mt_8 (bmat_param_8(C),  bmat_param_8(A),  bmat_param_8(B));
+extern void m4ri_wrap_mt_16(bmat_param_16(C), bmat_param_16(A), bmat_param_16(B));
+extern void m4ri_wrap_mt_32(bmat_param_32(C), bmat_param_32(A), bmat_param_32(B));
+extern void m4ri_wrap_mt_64(bmat_param_64(C), bmat_param_64(A), bmat_param_64(B));
+
+extern uint32_t m4ri_wrap_vm_8 (uint32_t V, bmat_param_8 (M));
+extern uint32_t m4ri_wrap_vm_16(uint32_t V, bmat_param_16(M));
+extern uint32_t m4ri_wrap_vm_32(uint32_t V, bmat_param_32(M));
+extern uint64_t m4ri_wrap_vm_64(uint64_t V, bmat_param_64(M));
+extern uint32_t m4ri_wrap_mv_8 (bmat_param_8 (M), uint32_t V);
+extern uint32_t m4ri_wrap_mv_16(bmat_param_16(M), uint32_t V);
+extern uint32_t m4ri_wrap_mv_32(bmat_param_32(M), uint32_t V);
+extern uint64_t m4ri_wrap_mv_64(bmat_param_64(M), uint64_t V);
