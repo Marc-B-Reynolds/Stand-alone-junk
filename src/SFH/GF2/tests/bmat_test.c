ef38dbec8691aa733e6c81113ac6693ac5263595
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2024-09-14 00:33:04 +0200

    Update bmat_test.c

--- src/SFH/GF2/tests/bmat_test.c
+++ src/SFH/GF2/tests/bmat_test.c
@@ -210,6 +210,100 @@ uint32_t test_all_binary_3param(prng_t* prng, uint32_t trials)
   return errors;
 }
 
+//******************************************************************************
+// matrix/vector products
+
+#if 1
+// the pair of functions to test
+typedef struct {
+  uint64_t (*f0)(uint64_t, uint64_t*);
+  uint64_t (*f1)(uint64_t, uint64_t*);
+} mvfunc_pair_t;
+
+// a full set of pairs for all sizes
+typedef struct {
+  char* name;
+  mvfunc_pair_t p[4];
+} mvfunc_set_t;
+
+// need wrappers
+uint64_t mvr_8 (uint64_t v, uint64_t* m) { return (uint64_t)bmat_mulv_8_ref (m,(uint8_t) v); }
+uint64_t mvr_16(uint64_t v, uint64_t* m) { return (uint64_t)bmat_mulv_16_ref(m,(uint16_t)v); }
+uint64_t mvr_32(uint64_t v, uint64_t* m) { return (uint64_t)bmat_mulv_32_ref(m,(uint32_t)v); }
+uint64_t mvr_64(uint64_t v, uint64_t* m) { return (uint64_t)bmat_mulv_64_ref(m,(uint64_t)v); }
+uint64_t mva_8 (uint64_t v, uint64_t* m) { return (uint64_t)bmat_mulv_8 (m,(uint8_t) v); }
+uint64_t mva_16(uint64_t v, uint64_t* m) { return (uint64_t)bmat_mulv_16(m,(uint16_t)v); }
+uint64_t mva_32(uint64_t v, uint64_t* m) { return (uint64_t)bmat_mulv_32(m,(uint32_t)v); }
+uint64_t mva_64(uint64_t v, uint64_t* m) { return (uint64_t)bmat_mulv_64(m,(uint64_t)v); }
+
+uint64_t vmr_8 (uint64_t v, uint64_t* m) { return (uint64_t)bmat_vmul_8_ref ((uint8_t) v,m); }
+uint64_t vmr_16(uint64_t v, uint64_t* m) { return (uint64_t)bmat_vmul_16_ref((uint16_t)v,m); }
+uint64_t vmr_32(uint64_t v, uint64_t* m) { return (uint64_t)bmat_vmul_32_ref((uint32_t)v,m); }
+uint64_t vmr_64(uint64_t v, uint64_t* m) { return (uint64_t)bmat_vmul_64_ref((uint64_t)v,m); }
+uint64_t vma_8 (uint64_t v, uint64_t* m) { return (uint64_t)bmat_vmul_8 ((uint8_t) v,m); }
+uint64_t vma_16(uint64_t v, uint64_t* m) { return (uint64_t)bmat_vmul_16((uint16_t)v,m); }
+uint64_t vma_32(uint64_t v, uint64_t* m) { return (uint64_t)bmat_vmul_32((uint32_t)v,m); }
+uint64_t vma_64(uint64_t v, uint64_t* m) { return (uint64_t)bmat_vmul_64((uint64_t)v,m); }
+
+
+static inline uint32_t test_mvprod(prng_t*        prng,
+				   test_fn_set_t* mset,
+				   uint64_t (*f0)(uint64_t, uint64_t*),
+				   uint64_t (*f1)(uint64_t, uint64_t*),
+				   uint32_t       n)
+{
+  uint64_t a[64];
+
+  mset->rsize();
+
+  uint64_t mask = UINT64_C(~0) >> (64-(mset->n));
+
+  for(uint32_t i=0; i<n; i++) {
+    mset->random(a,prng);
+
+    uint64_t v  = prng_u64(prng) & mask;
+    uint64_t r0 = f0(v,a);
+    uint64_t r1 = f1(v,a);
+
+    if (r0==r1) continue;
+    
+    return test_fail();
+  }
+  return test_pass();
+}
+
+//#define FUNC_SET(N) {.name = #N, .p={SET_P(N,8), SET_P(N,16), SET_P(N,32), SET_P(N,64)} }
+
+// table of matrix/vector product funcs
+mvfunc_set_t mvp_funcs[] = {
+  {.name="bmat_mulv_n",
+   .p={{.f0=mvr_8, .f1=mva_8},
+       {.f0=mvr_16,.f1=mva_16},
+       {.f0=mvr_32,.f1=mva_32},
+       {.f0=mvr_64,.f1=mva_64}}},
+  {.name="bmat_vmul_n",
+   .p={{.f0=vmr_8, .f1=vma_8},
+       {.f0=vmr_16,.f1=vma_16},
+       {.f0=vmr_32,.f1=vma_32},
+       {.f0=vmr_64,.f1=vma_64}}},
+};
+
+uint32_t test_all_mvprod(prng_t* prng, uint32_t trials)
+{
+  uint32_t errors = 0;
+
+  for(uint32_t i=0; i<(sizeof(mvp_funcs)/sizeof(mvp_funcs[0])); i++) {
+    mvfunc_set_t* set = mvp_funcs+i;
+    test_banner(set->name);
+
+    for(uint32_t t=0; t<4; t++) {
+      errors += test_mvprod(prng, fn_set[t], set->p[t].f0, set->p[t].f1, trials);
+    }
+  }
+
+  return errors;
+}
+#endif
 
 //******************************************************************************
 
@@ -231,6 +325,7 @@ int main(void)
   errors += test_all_unary_2param(&prng, trials);
   errors += test_all_unary_1param(&prng, trials);
   errors += test_all_binary_3param(&prng, trials);
+  errors += test_all_mvprod(&prng, trials);
   
   if (errors == 0)
     printf("all tests passed\n");
