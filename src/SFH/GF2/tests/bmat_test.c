14abd03bab62913ddb0218e40e93e34fdd973e28
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2024-09-18 13:40:43 +0200

    Update bmat_test.c

--- src/SFH/GF2/tests/bmat_test.c
+++ src/SFH/GF2/tests/bmat_test.c
@@ -19,7 +19,146 @@
 
 #define FUNC_SET(N) {.name = #N, .p={SET_P(N,8), SET_P(N,16), SET_P(N,32), SET_P(N,64)} }
 
+// functions that need wrappers for testing
+#define SET_PX(A,B,W)     {.f0=A##_##W,.f1=B##_##W}
+#define FUNC_SET_X(N,A,B) {.name = N, .p={SET_PX(A,B,8), SET_PX(A,B,16), SET_PX(A,B,32), SET_PX(A,B,64)} }
 
+// signatures
+//   gen_func       void     f(uint64_t*, uint64_t)
+//   u1func         void     f(uint64_t*)
+//   unary_2param   void     f(uint64_t*, uint64_t*)
+//   binary_3param  void     f(uint64_t*, uint64_t*, uint64_t*)
+//   mvprod         uint64_t f(uint64_t, uint64_t*)
+
+
+//******************************************************************************
+
+// the pair of functions to test
+typedef struct {
+  void (*f0)(uint64_t*, uint64_t);
+  void (*f1)(uint64_t*, uint64_t);
+} gen_func_pair_t;
+
+// a full set of pairs for all sizes
+typedef struct {
+  char* name;
+  gen_func_pair_t p[4];
+} gen_func_set_t;
+
+
+static inline uint32_t test_gen(prng_t*        prng,
+				test_fn_set_t* mset,
+				void (*f0)(uint64_t*, uint64_t),
+				void (*f1)(uint64_t*, uint64_t),
+				uint32_t       n)
+{
+  uint64_t r0[64],r1[64];
+  
+  mset->rsize();
+
+  uint64_t mask = UINT64_C(~0) >> (64-(mset->n));
+  
+  for(uint32_t i=0; i<n; i++) {
+    uint64_t v  = prng_u64(prng) & mask;
+
+    f0(r0,v);
+    f1(r1,v);
+
+    if (mset->equal(r0,r1)) continue;
+    
+    return test_fail();
+  }
+  return test_pass();
+}
+
+// "dumbest" version ever is perfect for our purposes
+// (only used for 8 & 16 bit testing)
+uint32_t my_first_carryless_product_32(uint32_t a, uint32_t b)
+{
+  uint32_t r = 0;
+
+  // not going to check which has smaller popcount
+  while (b != 0) {
+
+    // not going to fast-forward past trailing zeros
+    if (b & 1)
+      r ^= a;
+
+    a <<= 1;
+    b >>= 1;
+  }
+
+  // because it just doesn't matter! (yes I'm attempting
+  // to amuse myself with hacking-n-slashing testing code)
+  return r;
+}
+
+
+#if 0
+// humm..don't have to matrix from function for 8 & 16 ATM
+void wcl_8 (uint64_t* m, uint64_t v) { bmat_set_cl_mul_8 (m,(uint8_t) v); }
+void wcl_16(uint64_t* m, uint64_t v) { bmat_set_cl_mul_16(m,(uint16_t)v); }
+void wcl_32(uint64_t* m, uint64_t v) { bmat_set_cl_mul_32(m,(uint32_t)v); }
+void wcl_64(uint64_t* m, uint64_t v) { bmat_set_cl_mul_64(m,(uint64_t)v); }
+
+void wcc_8 (uint64_t* m, uint64_t v) { bmat_set_cc_mul_8 (m,(uint8_t) v); }
+void wcc_16(uint64_t* m, uint64_t v) { bmat_set_cc_mul_16(m,(uint16_t)v); }
+void wcc_32(uint64_t* m, uint64_t v) { bmat_set_cc_mul_32(m,(uint32_t)v); }
+void wcc_64(uint64_t* m, uint64_t v) { bmat_set_cc_mul_64(m,(uint64_t)v); }
+
+void wcr_8 (uint64_t* m, uint64_t v) { bmat_set_cr_mul_8 (m,(uint8_t) v); }
+void wcr_16(uint64_t* m, uint64_t v) { bmat_set_cr_mul_16(m,(uint16_t)v); }
+void wcr_32(uint64_t* m, uint64_t v) { bmat_set_cr_mul_32(m,(uint32_t)v); }
+void wcr_64(uint64_t* m, uint64_t v) { bmat_set_cr_mul_64(m,(uint64_t)v); }
+
+#else
+
+uint8_t  id_8 (uint8_t  x) { return x; }
+uint16_t id_16(uint16_t x) { return x; }
+uint32_t id_32(uint32_t x) { return x; }
+uint64_t id_64(uint64_t x) { return x; }
+
+// temp hack: too lazy to make unused macro
+#if defined(__GNUC__)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#endif
+void wid_8 (uint64_t* m, uint64_t v) { bmat_set_unit_8 (m); }
+void wid_16(uint64_t* m, uint64_t v) { bmat_set_unit_16(m); }
+void wid_32(uint64_t* m, uint64_t v) { bmat_set_unit_32(m); }
+void wid_64(uint64_t* m, uint64_t v) { bmat_set_unit_64(m); }
+
+void fid_8 (uint64_t* m, uint64_t v) { bmat_from_ufunc_8 (m,id_8 ); }
+void fid_16(uint64_t* m, uint64_t v) { bmat_from_ufunc_16(m,id_16); }
+void fid_32(uint64_t* m, uint64_t v) { bmat_from_ufunc_32(m,id_32); }
+void fid_64(uint64_t* m, uint64_t v) { bmat_from_ufunc_64(m,id_64); }
+
+#if defined(__GNUC__)
+#pragma GCC diagnostic pop
+#endif
+
+#if 1
+gen_func_set_t gen_funcs[] = {
+  FUNC_SET_X("set_unit",wid,fid),
+};
+
+uint32_t test_all_gen(prng_t* prng, uint32_t trials)
+{
+  uint32_t errors = 0;
+
+  for(uint32_t i=0; i<(sizeof(gen_funcs)/sizeof(gen_funcs[0])); i++) {
+    gen_func_set_t* set = gen_funcs+i;
+    test_banner(set->name);
+
+    for(uint32_t t=0; t<4; t++) {
+      errors += test_gen(prng, fn_set[t], set->p[t].f0, set->p[t].f1, trials);
+    }
+  }
+
+  return errors;
+}
+#endif
+#endif
 
 //******************************************************************************
 // validate two unary functions are the same: f0(m) = f1(m)
@@ -213,7 +352,6 @@ uint32_t test_all_binary_3param(prng_t* prng, uint32_t trials)
 //******************************************************************************
 // matrix/vector products
 
-#if 1
 // the pair of functions to test
 typedef struct {
   uint64_t (*f0)(uint64_t, uint64_t*);
@@ -272,20 +410,11 @@ static inline uint32_t test_mvprod(prng_t*        prng,
   return test_pass();
 }
 
-//#define FUNC_SET(N) {.name = #N, .p={SET_P(N,8), SET_P(N,16), SET_P(N,32), SET_P(N,64)} }
 
 // table of matrix/vector product funcs
 mvfunc_set_t mvp_funcs[] = {
-  {.name="bmat_mulv_n",
-   .p={{.f0=mvr_8, .f1=mva_8},
-       {.f0=mvr_16,.f1=mva_16},
-       {.f0=mvr_32,.f1=mva_32},
-       {.f0=mvr_64,.f1=mva_64}}},
-  {.name="bmat_vmul_n",
-   .p={{.f0=vmr_8, .f1=vma_8},
-       {.f0=vmr_16,.f1=vma_16},
-       {.f0=vmr_32,.f1=vma_32},
-       {.f0=vmr_64,.f1=vma_64}}},
+  FUNC_SET_X("bmat_mulv_n", mvr, mva),
+  FUNC_SET_X("bmat_vmul_n", vmr, vma),
 };
 
 uint32_t test_all_mvprod(prng_t* prng, uint32_t trials)
@@ -303,7 +432,6 @@ uint32_t test_all_mvprod(prng_t* prng, uint32_t trials)
 
   return errors;
 }
-#endif
 
 //******************************************************************************
 
@@ -326,6 +454,7 @@ int main(void)
   errors += test_all_unary_1param(&prng, trials);
   errors += test_all_binary_3param(&prng, trials);
   errors += test_all_mvprod(&prng, trials);
+  errors += test_all_gen(&prng, trials);
   
   if (errors == 0)
     printf("all tests passed\n");
