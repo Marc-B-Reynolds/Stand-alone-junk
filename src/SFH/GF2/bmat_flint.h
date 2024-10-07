e69273a2f02f7aa1e0a4ad9cbe3b74aab964ea4f
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2024-10-07 15:14:24 +0200

    Create bmat_flint.h

---
+++ src/SFH/GF2/bmat_flint.h
@@ -0,0 +1,8 @@
+// Marc B. Reynolds, 2022-2024
+// Public Domain under http://unlicense.org, see link for details.
+
+#pragma once
+
+#include <flint/fmpz_mod_poly.h>
+#include <flint/fmpz_mod_mat.h>
+#include <flint/nmod_mat.h>
