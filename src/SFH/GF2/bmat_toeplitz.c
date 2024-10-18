e0d5ee0b28db9dc041949446e20c8d179cd48e44
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2024-10-18 23:53:34 +0200

    Update bmat_toeplitz.c

--- src/SFH/GF2/bmat_toeplitz.c
+++ src/SFH/GF2/bmat_toeplitz.c
@@ -7,6 +7,85 @@
 //   using function to matrix. but have an equivalent here for compile
 //   time switch fallback.
 
+/// ## bmat_set_ut_toeplitz_*n*(m,k)
+/// Sets `m` to an upper triangluar Toeplitz matrix:
+///
+/// $$
+///   \left[ {\begin{array}{ccccc}
+///    k_{0} & k_{1}  & k_{2}  & \cdots & k_{n-1}  \newline
+///       0  & k_{0}  & k_{1}  & \cdots & k_{n-2}  \newline
+///       0  &    0   & k_{0}  & \cdots & k_{n-3}  \newline
+///   \vdots & \vdots & \vdots & \ddots & \vdots   \newline
+///       0  &    0   & \cdots & \cdots & k_{0}    \newline
+///  \end{array} } \right]
+/// $$
+///
+/// <details markdown="1"><summary>function list:</summary>
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ c
+/// void bmat_set_ut_toeplitz_8 (bmat_param_8(m),  uint64_t k)
+/// void bmat_set_ut_toeplitz_16(bmat_param_16(m), uint64_t k)
+/// void bmat_set_ut_toeplitz_32(bmat_param_32(m), uint64_t k)
+/// void bmat_set_ut_toeplitz_64(bmat_param_64(m), uint64_t k)
+///~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+/// </details>
+
+void bmat_set_ut_toeplitz_8(bmat_param_8(m), uint64_t k)
+{
+  k   &= 0xff;
+  k    = BMAT_MAIN_DIAGONAL_MASK_8 * k;
+  m[0] = k & UINT64_C(0x80c0e0f0f8fcfeff);
+}
+
+void bmat_set_ut_toeplitz_16(bmat_param_16(m), uint64_t k)
+{
+  // 4 rows/per 64-bit
+  uint64_t mask = UINT64_C(0xfff0fff0fff0fff0);
+
+  k &= 0xffff;
+  k  = (BMAT_MAIN_DIAGONAL_MASK_16 * k) & UINT64_C(0xfff8fffcfffeffff);
+
+  m[0] = k; k = (k << 4) & mask;
+  m[1] = k; k = (k << 4) & mask;
+  m[2] = k; k = (k << 4) & mask;
+  m[3] = k;
+}
+
+void bmat_set_ut_toeplitz_32(bmat_param_32(m), uint64_t k)
+{
+  // 2 rows/per 64-bit
+  uint64_t mask = UINT64_C(0xfffffffcfffffffc);
+
+  k &= 0xffffffff;
+  k  = (BMAT_MAIN_DIAGONAL_MASK_32 * k) & UINT64_C(0xfffffffeffffffff);
+
+  for(uint32_t i=0; i<16; i++) {
+    m[i] = k; k = (k << 2) & mask;
+  }
+}
+
+void bmat_set_ut_toeplitz_64(bmat_param_64(m), uint64_t k)
+{
+  for(uint32_t i=0; i<64; i++) {
+    m[i] = k; k <<= 1;
+  }
+}
+
+
+/// ## bmat_set_lt_toeplitz_*n*(m,k)
+/// Sets `m` to an lower triangluar Toeplitz matrix:
+///
+/// put figure here
+///
+/// <details markdown="1"><summary>function list:</summary>
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ c
+/// void bmat_set_lt_toeplitz_8 (bmat_param_8(m),  uint64_t k)
+/// void bmat_set_lt_toeplitz_16(bmat_param_16(m), uint64_t k)
+/// void bmat_set_lt_toeplitz_32(bmat_param_32(m), uint64_t k)
+/// void bmat_set_lt_toeplitz_64(bmat_param_64(m), uint64_t k)
+///~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+/// </details>
+
+
 // these compile like garbage
 
 void bmat_set_toeplitz_8_i(bmat_param_8(m), uint32_t v)
@@ -45,6 +124,7 @@ void bmat_set_toeplitz_32_i(bmat_param_32(m), uint64_t v)
   array_to_bmat_32(m,a);
 }  
 
+
 void bmat_set_toeplitz_8 (bmat_param_8 (m), uint8_t  r, uint8_t  c) { bmat_set_toeplitz_8_i (m, (((uint32_t)r)<< 8)|bit_reverse_8 (c)); }
 void bmat_set_toeplitz_16(bmat_param_16(m), uint16_t r, uint16_t c) { bmat_set_toeplitz_16_i(m, (((uint32_t)r)<<16)|bit_reverse_16(c)); }
 void bmat_set_toeplitz_32(bmat_param_32(m), uint32_t r, uint32_t c) { bmat_set_toeplitz_32_i(m, (((uint64_t)r)<<32)|bit_reverse_32(c)); }
@@ -61,7 +141,7 @@ void bmat_set_toeplitz_64(bmat_param_64(m), uint64_t r, uint64_t c)
 }
 
 
-// reverify (well add to test)
+// forward to LT
 #if 1
 void bmat_set_cl_mul_8 (bmat_param_8 (m), uint8_t  n) { bmat_set_toeplitz_8 (m,0,n); }
 void bmat_set_cl_mul_16(bmat_param_16(m), uint16_t n) { bmat_set_toeplitz_16(m,0,n); }
@@ -79,6 +159,9 @@ void bmat_set_cc_mul_16(bmat_param_16(m), uint16_t n) { bmat_set_toeplitz_16(m,b
 void bmat_set_cc_mul_32(bmat_param_32(m), uint32_t n) { bmat_set_toeplitz_32(m,bit_reverse_32(n),n); }
 void bmat_set_cc_mul_64(bmat_param_64(m), uint64_t n) { bmat_set_toeplitz_64(m,bit_reverse_64(n),n); }
 
+
+
+// change to using ut_toeplitz
 #if 1
 void bmat_set_cr_mul_8 (bmat_param_8 (m), uint8_t  n) { uint8_t  a[ 8]; n = bit_reverse_8 (n); for_range(i,0, 8) { a[i] = n; n <<= 1; } array_to_bmat_8 (m,a); }
 void bmat_set_cr_mul_16(bmat_param_16(m), uint16_t n) { uint16_t a[16]; n = bit_reverse_16(n); for_range(i,0,16) { a[i] = n; n <<= 1; } array_to_bmat_16(m,a); }
