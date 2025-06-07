8414659f4f805215f4a5ed69fb6ae07eabc2c17f
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2025-06-07 10:50:43 +0200

    added A_3 lattice (and lack of fixed-point comment)

--- src/SFH/simd/lattice_2d3d.h
+++ src/SFH/simd/lattice_2d3d.h
@@ -2,22 +2,31 @@
 // Marc B. Reynolds, 2017-2025
 // Public Domain under http://unlicense.org, see link for details.
 
+// Nearest lattice points for:
+//   2D: D₂
+//   3D: D₃,A₃
+// Z lattice..what's the point of a routine?
+
 #pragma once
 
+// On lattice points routines:
+// should implement versions that jump directly to fixed point.
+// that has some gotchas is why I haven't bothered yet. example
+// binary32 type we can expect hardware op to convert to signed
+// integer which leaves us with 31 bits. but anyway these float
+// based versions perform 2 float→int and 1 int→float which
+// is a bit sad but we're exact.
 
 //*******************************************************
 // D_2 lattice: (checkerboard, diagonal square, centered square)
 // • covering radius (R) is 1
+// • odd number of bits to quantize (2b+1 bits with b per component)
+// • quantization max distance error: 
 // • "Fast Quantizing and Decoding Algorithms for Lattice Quantizers
 //    and Codes", Conway & Sloane, 1982 (algorithm 2)
 
-// NOTE: that going straight to integer is possible but will increase
-// quantization error for small inputs. Example is if all targets
-// don't support direct floating-point to unsigned integer conversions
-// then we lose a bit by being required to fit in signed. In that case
-// for encoding to 31 bits we need a 16-bit integer part. But anyway
-// 
-
+// returns the lattice coordinate (the sum of coordinates is even parity)
+// direct result is convenient for quantization
 static inline i32x2_t vec2f_lattice_nearest_d2(vec2f_t v)
 {
   i32x2_t r = vec2_to_int(v);                // 'v' truncated as integer
@@ -65,9 +74,11 @@ static inline i64x2_t vec2d_lattice_nearest_d2(vec2d_t v)
 }
 
 //*******************************************************
-// D_3 lattice:
+// D_3 lattice: face-centered cubic (FCC)
 // • covering radius (R) is 1
 // • same as D_2 (just one more dimension)
+// • 3b+2 bits to quantize with b per component
+// • quantization max distance error: 
 
 static inline i32x4_t vec3f_lattice_nearest_d3(vec3f_t v)
 {
@@ -107,3 +118,47 @@ static inline i64x4_t vec3d_lattice_nearest_d3(vec3d_t v)
   return r;
 }
 
+//*******************************************************
+// A_3 lattice: body-centered cubic (BCC)
+// • covering radius (R) is sqrt(5)/4
+// • 3b+1 bits to quantize with b per component
+// • quantization max distance error: 2^{-b} R
+
+// these don't directly return the lattice coordinate which
+// is: let 'r' be result then coordinate is 2r + s
+// where 's' broadcast (all zeros or ones)
+static inline i32x4_t vec3f_lattice_nearest_a3(vec3f_t v, bool* s)
+{
+  vec3f_t  fv = vec3_floor(v);                    // a base cell corner
+  vec3f_t  nv = vec3_round_up(v);                 // nearest corner
+  
+  // compute the two possible distances and choose smaller
+  // (standard because the wrap hasn't occurred yet for tiled)
+  float    d0 = vec3_dist_sq(fv+0.5f, v);         // dist squared to center
+  float    d1 = vec3_dist_sq(nv, v);              // dist squared to closest corner
+  bool     t  = d0 < d1;                          //   which?
+  vec3f_t  qv = t ? fv : nv;                      // select closest
+  i32x4_t  q  = vec3_to_int(qv);                  // convert to integer
+
+  *s = t;
+  
+  return q;
+}
+
+static inline i64x4_t vec3d_lattice_nearest_a3(vec3d_t v, bool* s)
+{
+  vec3d_t  fv = vec3_floor(v);                    // a base cell corner
+  vec3d_t  nv = vec3_round_up(v);                 // nearest corner
+  
+  // compute the two possible distances and choose smaller
+  // (standard because the wrap hasn't occurred yet for tiled)
+  double   d0 = vec3_dist_sq(fv+0.5f, v);         // dist squared to center
+  double   d1 = vec3_dist_sq(nv, v);              // dist squared to closest corner
+  bool     t  = d0 < d1;                          //   which?
+  vec3d_t  qv = t ? fv : nv;                      // select closest
+  i64x4_t  q  = vec3_to_int(qv);                  // convert to integer
+
+  *s = t;
+  
+  return q;
+}
