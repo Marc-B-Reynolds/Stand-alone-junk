8752f3248ee03e738485c2fd84714daa132e1b50
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2026-02-01 18:20:01 +0100

    Update intops.h

--- src/SFH/intops.h
+++ src/SFH/intops.h
@@ -134,47 +134,40 @@ static inline uint64_t sub_mod_n_u64(uint64_t x, uint64_t y, uint64_t n)
 }
 
 
+// 2^64 mod k
+static inline uint64_t mod_k_base_u64(uint64_t k)
+{
+  static const uint64_t N = UINT64_C(1) << 63;
+  
+  if (k <= N) {
+    uint64_t r = (N % k) << 1;
+    
+    return r < k ? r : r-k;
+  }
+  
+  return -k;  
+}
 
+// 2^32 mod k
+static inline uint32_t mod_k_base_u32(uint32_t k)
+{
+#if 1
+  static const uint32_t N = UINT32_C(1) << 31;
+  
+  if (k <= N) {
+    uint32_t r = (N % k) << 1;
+    
+    return r < k ? r : r-k;
+  }
+  
+  return -k;
+#else
+  static const uint64_t N = UINT64_C(1) << 32;
+  return (uint32_t)(N % k);
+#endif
+}
 
 //────────────────────────────────────────────────────────────────────────────────────
-// multiplicative inverse: find i such that i*a = 1 (requires odd 'a')
-// SEE: https://marc-b-reynolds.github.io/math/2017/09/18/ModInverse.html
-
-static inline uint32_t mod_inverse_u32(uint32_t a)
-{
-  uint32_t x = (3*a)^2; 
-  uint32_t y = 1 - a*x;
-  x = x*(1+y); y *= y;
-  x = x*(1+y); y *= y;
-  x = x*(1+y);
-  return x;
-}
-
-static inline uint64_t mod_inverse_u64(uint64_t a)
-{
-  uint64_t x = (3*a)^2; 
-  uint64_t y = 1 - a*x;
-  x = x*(1+y); y *= y;
-  x = x*(1+y); y *= y;
-  x = x*(1+y); y *= y;
-  x = x*(1+y);
-  return x;
-}
-
-// two for one trick: mod inverse of a & b (pair order as input)
-static inline pair_u32_t mod_inverse_x2_u32(uint32_t a, uint32_t b)
-{
-  uint32_t c  = a*b;
-  uint32_t ic = mod_inverse_u32(c);
-  return pair_u32(ic*b, ic*a);
-}
-
-static inline pair_u64_t mod_inverse_x2_u64(uint64_t a, uint64_t b)
-{
-  uint64_t c  = a*b;
-  uint64_t ic = mod_inverse_u64(c);
-  return pair_u64(ic*b, ic*a);
-}
 
 // 128-bit result of 64-bit product (access via hi and lo elements)
 static inline pair_u64_t mul_hilo_64(uint64_t a, uint64_t b)
@@ -210,7 +203,47 @@ extern uint64_t gcd_nz_u64(uint64_t u, uint64_t v);
 extern gcd_u64_t gcd_extended_u64(uint64_t u, uint64_t v);
 extern gcd_u32_t gcd_extended_u32(uint32_t u, uint32_t v);
 
+extern const uint8_t mod_inverse_table_8[];
+
 #else
+
+// 8-bit mod inverse table. only odd elements accessed for legal input
+// (could shift and half the same of the table..but meh)
+const uint8_t mod_inverse_table_8[] =
+{
+  0,0x01,0,0xab,0,0xcd,0,0xb7,
+  0,0x39,0,0xa3,0,0xc5,0,0xef,
+  0,0xf1,0,0x1b,0,0x3d,0,0xa7,
+  0,0x29,0,0x13,0,0x35,0,0xdf,
+  0,0xe1,0,0x8b,0,0xad,0,0x97,
+  0,0x19,0,0x83,0,0xa5,0,0xcf,
+  0,0xd1,0,0xfb,0,0x1d,0,0x87,
+  0,0x09,0,0xf3,0,0x15,0,0xbf,
+  0,0xc1,0,0x6b,0,0x8d,0,0x77,
+  0,0xf9,0,0x63,0,0x85,0,0xaf,
+  0,0xb1,0,0xdb,0,0xfd,0,0x67,
+  0,0xe9,0,0xd3,0,0xf5,0,0x9f,
+  0,0xa1,0,0x4b,0,0x6d,0,0x57,
+  0,0xd9,0,0x43,0,0x65,0,0x8f,
+  0,0x91,0,0xbb,0,0xdd,0,0x47,
+  0,0xc9,0,0xb3,0,0xd5,0,0x7f,
+  0,0x81,0,0x2b,0,0x4d,0,0x37,
+  0,0xb9,0,0x23,0,0x45,0,0x6f,
+  0,0x71,0,0x9b,0,0xbd,0,0x27,
+  0,0xa9,0,0x93,0,0xb5,0,0x5f,
+  0,0x61,0,0x0b,0,0x2d,0,0x17,
+  0,0x99,0,0x03,0,0x25,0,0x4f,
+  0,0x51,0,0x7b,0,0x9d,0,0x07,
+  0,0x89,0,0x73,0,0x95,0,0x3f,
+  0,0x41,0,0xeb,0,0x0d,0,0xf7,
+  0,0x79,0,0xe3,0,0x05,0,0x2f,
+  0,0x31,0,0x5b,0,0x7d,0,0xe7,
+  0,0x69,0,0x53,0,0x75,0,0x1f,
+  0,0x21,0,0xcb,0,0xed,0,0xd7,
+  0,0x59,0,0xc3,0,0xe5,0,0x0f,
+  0,0x11,0,0x3b,0,0x5d,0,0xc7,
+  0,0x49,0,0x33,0,0x55,0,0xff
+};
 
 #define INTOPS_SWAP(T,X,Y) { T t = X; X=Y; Y=t; }
 
@@ -305,6 +338,47 @@ static inline uint64_t gcd_u64(uint64_t a, uint64_t b)
     return gcd_nz_u64(a,b);
 
   return a+b;
+}
+
+
+//────────────────────────────────────────────────────────────────────────────────────
+// multiplicative inverse: find i such that i*a = 1 (requires odd 'a')
+// SEE: https://marc-b-reynolds.github.io/math/2017/09/18/ModInverse.html
+
+static inline uint32_t mod_inverse_u32(uint32_t a)
+{
+  uint32_t x = (3*a)^2; 
+  uint32_t y = 1 - a*x;
+  x = x*(1+y); y *= y;
+  x = x*(1+y); y *= y;
+  x = x*(1+y);
+  return x;
+}
+
+static inline uint64_t mod_inverse_u64(uint64_t a)
+{
+  uint64_t x = (3*a)^2; 
+  uint64_t y = 1 - a*x;
+  x = x*(1+y); y *= y;
+  x = x*(1+y); y *= y;
+  x = x*(1+y); y *= y;
+  x = x*(1+y);
+  return x;
+}
+
+// two for one trick: mod inverse of a & b (pair order as input)
+static inline pair_u32_t mod_inverse_x2_u32(uint32_t a, uint32_t b)
+{
+  uint32_t c  = a*b;
+  uint32_t ic = mod_inverse_u32(c);
+  return pair_u32(ic*b, ic*a);
+}
+
+static inline pair_u64_t mod_inverse_x2_u64(uint64_t a, uint64_t b)
+{
+  uint64_t c  = a*b;
+  uint64_t ic = mod_inverse_u64(c);
+  return pair_u64(ic*b, ic*a);
 }
 
 #undef INTOPS_SWAP
