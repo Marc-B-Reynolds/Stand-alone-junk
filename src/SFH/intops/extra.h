f12abe366747ee4bb74b1af10b67e46bd39a0f45
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2026-09-17 23:15:10 +0200

    Update extra.h

--- src/SFH/intops/extra.h
+++ src/SFH/intops/extra.h
@@ -10,3 +10,5 @@ extern void     binomial_init(void);
 extern uint64_t binomial_u64(uint64_t n, uint64_t k);
 extern uint64_t binomial_i64(int64_t  n, int64_t  k);
 
+// prime.c
+extern bool is_prime_u32(uint32_t n);
